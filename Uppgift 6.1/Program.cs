a1f72dce967347a3b064be33b52fe4b5013f07d8
Admin <Admin@TEB127030>
2023-05-12 08:49:30 +0200

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace uppgift_6._1
+{
+    class program
+    {
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int x = tal1 + tal2 + tal3;
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv tre heltal.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            int svar = Addera(x, y, z);
+            Console.WriteLine();
+            Console.WriteLine("Adderar man alla talen med varandra får man:" + x + "+" + y + "+" + z + "=" + svar);
+        }
+    }
+}
